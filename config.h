7346e8fdef6ce98b32dee75127b3cd928f108c63
ryukamish <10929626-ryukamish@users.noreply.github.com>
2022-04-02 21:17:08 +0530

    Added few more stuff

--- config.h
+++ config.h
@@ -65,9 +65,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { netspeed_rx,  " [  %s ] ",           "enp0s3" },
-    { cpu_perc,     " [ CPU  %s% ] ",      NULL },
-    { ram_used,     " [ MEM %s ] ",         NULL },
-	{ datetime,     " [ %s ] ",             " %x | %r " },
+    { netspeed_rx,  "[  %s ",            "enp0s3" },
+    { netspeed_tx,  "/  %s ]",             "enp0s3" },
+    { cpu_perc,     " [ CPU  %s% ]",      NULL },
+    { ram_used,     " [ MEM: %s ]",        NULL },
+	{ datetime,     " [ %s ]",             "  %x  %r " },
 };
 
