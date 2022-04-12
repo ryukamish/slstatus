58f5975026a276ea10ee8279dbe8c33d15e5fd3c
ryukamish <10929626-ryukamish@users.noreply.github.com>
2022-04-12 16:01:10 +0530

    battery icon changed and made it work

--- config.h
+++ config.h
@@ -65,12 +65,13 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format                      argument */
-    { netspeed_rx,  "[  %s ",              "enp0s3" },
-    { netspeed_tx,  "/  %s ]",             "enp0s3" },
-    { battery_perc, " [  %s% ]",           "BAT0" },
-    { vol_perc,     " [ %s ]",              "/dev/mixer" },
-    { cpu_perc,     " [  %s% ]",           NULL },
-    { ram_used,     " [  %s ]",            NULL },
-	{ datetime,     " [ %s ]",              " %b %d %Y  %I:%M %p" },
+    { netspeed_rx,          "[  %s ",              "enp0s3" },
+    { netspeed_tx,          "/  %s ]",             "enp0s3" },
+    { battery_state,        "[ %s ",                "BAT0" },
+    { battery_perc,         "%s% ]",             "BAT0" },
+    { vol_perc,             " [ %s ]",              "/dev/mixer" },
+    { cpu_perc,             " [  %s% ]",           NULL },
+    { ram_used,             " [  %s ]",            NULL },
+	{ datetime,             " [ %s ]",              " %b %d %Y  %I:%M %p" },
 };
 
